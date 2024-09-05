1cdc80d53694b52db973ea1fc82c4a682c875fed
Dicksonkibet <ngetichdickson64@gmail.com>
2024-09-05 15:53:33 +0300

    Update app-routing.module.ts

--- merumarketplace/src/app/app-routing.module.ts
+++ merumarketplace/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Authentication/login/login.component';
 import { RegistrationComponent } from './Authentication/registration/registration.component';
 import { LandingComponent } from './Authentication/landing/landing.component';
+import { leadingComment } from '@angular/compiler';
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent},
-  {path:'Registration', component: RegistrationComponent},
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  //{path:'login', component: LoginComponent},
+  //{path:'Registration', component: RegistrationComponent},
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  {path:'landing', component: LandingComponent},
+
 ];
 
 @NgModule({
