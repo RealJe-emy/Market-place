166a8845d0d04a7857e7a751998a57024a26ba48
Brian Agwata <agwatabrian17@gmail.com>
2024-09-25 16:02:17 +0300

    updates
    updates

--- merumarketplace/src/app/pages/jewellery/women jewellery/womenjewellerypage/womenjewellerypage.component.ts
+++ merumarketplace/src/app/pages/jewellery/women jewellery/womenjewellerypage/womenjewellerypage.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './womenjewellerypage.component.scss'
 })
 export class WomenjewellerypageComponent {
+searchResultTitle: any;
+minPrice: any;
+maxPrice: any;
+applyPriceFilter() {
+throw new Error('Method not implemented.');
+}
+filterByCategory(_t16: any) {
+throw new Error('Method not implemented.');
+}
+filterByMaterial(_t23: any) {
+throw new Error('Method not implemented.');
+}
+filterByBrand(_t30: any) {
+throw new Error('Method not implemented.');
+}
+sortBy(_t36: any) {
+throw new Error('Method not implemented.');
+}
+currentPage: any;
+totalPages: any;
+previousPage() {
+throw new Error('Method not implemented.');
+}
+nextPage() {
+throw new Error('Method not implemented.');
+}
 
 }
